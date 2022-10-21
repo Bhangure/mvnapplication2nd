bd48a5fd951de9f7baebb297d6348dceeeeac77a
Bhangure <bhangureasha853@gmai.com>
2022-10-21 08:53:38 +0530

    this is my maven 2nd application

---
+++ src/test/java/com/sample/sample/SampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sample.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("this is test cases");
+	}
+
+}
